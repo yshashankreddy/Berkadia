6f65840c5cbcfc3e9c4dc0884738c767a9d8d822
yshashankreddy <35621878+yshashankreddy@users.noreply.github.com>
2018-11-30 19:25:41 +0530

    Create Settimeout.js

---
+++ Week3/Settimeout.js
@@ -0,0 +1,12 @@
+function myFunc(arg) {
+  console.log(`arg was => ${arg}`);
+}
+
+setTimeout(myFunc, 1500, 'Shashank');
+
+/*OUTPUT:
+
+
+C:\Users\SHASHANK\Desktop\week3\my-app>node settimeout.js
+arg was => Shashank
+*/
