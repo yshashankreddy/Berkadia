a41ca501ce06a650101bd7b8486d66852709e4a8
yshashankreddy <35621878+yshashankreddy@users.noreply.github.com>
2018-11-30 19:37:55 +0530

    Add files via upload

---
+++ Week3/stringfunctions.js
@@ -0,0 +1,13 @@
+var str=new String("Shashank is in Berkedia");
+console.log(str);
+console.log(str.length);
+console.log(str.indexOf("Berkedia"));
+console.log(str.search("Berkedia"));
+console.log(str.slice(0,8));
+console.log(str.substr(9));
+console.log(str.replace("Shashank","Ganesh"));
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.concat(" ","with"," ","Ganesh"));
+console.log(str.charAt(0));
+console.log(str.split(" "));
