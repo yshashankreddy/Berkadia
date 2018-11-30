e9a79757a122a28f1be77432fb9471eff7727093
yshashankreddy <35621878+yshashankreddy@users.noreply.github.com>
2018-11-30 19:12:50 +0530

    Create Example.js

---
+++ Week3/Object Creation/Example.js
@@ -0,0 +1,20 @@
+ class Person { 
+	constructor(name) { 
+		this.name = name; 
+	} 
+	toString_Person() { 
+		return "Name of person = " + this.name; 
+	} 
+} 
+class Student extends Person { 
+	constructor(name, Sid) {  
+		super(name);  
+		this.Sid = Sid; 
+	} 
+	toString() { 
+		return super.toString_Person() + ", Student Id = "
+			+ this.Sid 
+	} 
+}  
+var Student_1 = new Student("Shashank", "16"); 
+console.log(Student_1.toString()); 
