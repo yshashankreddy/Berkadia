bc4f8df59aff4dd690af3b362873b3eabdbcdcb7
Shashank Reddy <35621878+yshashankreddy@users.noreply.github.com>
2018-12-07 18:50:23 +0530

    Create Total.js

---
+++ Week4/Node..js Frameworks/Total.js
@@ -0,0 +1,9 @@
+Total.js:
+--------
+
+
+              Total.js is one of the modern and modular Node.js frameworks supporting model-view controller (MVC) software architecture. 
+It is fully compatible with various client-side frameworks, like Angular.js, Polymer, Backbone.js, Bootstrap and more. 
+Total.js is fully extensible and asynchronous. 
+               One great feature of Total.js is that you don't need any tools like Grunt to compress JavaScript, HTML and CSS. Additionally, the framework has NoSQL embedded database and supports array and other prototypes. 
+It supports RESTful routing mechanism, supports web sockets, media streaming and more.
