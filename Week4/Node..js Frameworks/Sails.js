de24d9d91475172d17183ec8508be2794508cac5
Shashank Reddy <35621878+yshashankreddy@users.noreply.github.com>
2018-12-07 18:48:34 +0530

    Create Sails.js

---
+++ Week4/Node..js Frameworks/Sails.js
@@ -0,0 +1,9 @@
+Sails.js:
+--------
+
+
+            Sails.js is one of the most popular real-time frameworks around for building Node.js applications. Sails.js offers a model-view controller (MVC) pattern for implementing data-driven application programming interfaces (APIs).
+The framework has gained ground for building real-time chat applications, dashboards and multiplayer games. 
+             It uses Waterline for object-relational mapping and providing database solutions. Sails.js is built on top of Node.js and uses Express.js for handling HTTP requests. 
+It is ideal for creating browser-based applications as it is compatible with all the Grunt modules, including LESS, SASS, Stylus, CoffeeScript, Jade, Dust, and more.
+Sails.js supports any front-end approach, such as Angular, Backbone, iOS/ObjC, Android/Java or anything else.
