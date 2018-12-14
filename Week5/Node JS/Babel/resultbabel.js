66398f82b34ffc21ce85cda5390d9c346b7201ab
Shashank Reddy <35621878+yshashankreddy@users.noreply.github.com>
2018-12-14 20:17:27 +0530

    Create resultbabel.js

---
+++ Week5/Node JS/Babel/resultbabel.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var x = [shashank, sai];
+
+var myname = "SHASHANK REDDY";
+
+var odds = evens.map(function (x) {
+  return x + 1;
+});
+
+var nums = evens.map(function (x, i) {
+  return x + i;
+});
